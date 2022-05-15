62440fdfbab0822ce8e1fdd51f888eca1c19f784
Ryan Andersen <ryan.andersen05@gmail.com>
2022-05-15 00:27:54 -0400

    Getting Projectiles started

--- Assets/Scripts/Towers/EHTowerUnit.cs
+++ Assets/Scripts/Towers/EHTowerUnit.cs
@@ -5,16 +5,64 @@ using UnityEngine;
 
 public class EHTowerUnit : EHActor
 {
+    [SerializeField]
+    private float RotationSpeed = 100f;
+    [SerializeField]
+    private Transform RotationTransform;
+
+    private List<EHActor> Targets = new List<EHActor>();
     private EHActor EnemyTarget;
     
     #region monobehaviour methods
-    public void Update()
+    private void Update()
     {
+        if (EnemyTarget != null)
+        {
+            UpdateRotation();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        EHMinion Minion = other.GetComponent<EHMinion>();
+        if (Minion != null)
+        {
+            if (!Targets.Contains(Minion))
+            {
+                Targets.Add(Minion);
+                EnemyTarget = Targets[0];
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        EHMinion Minion = other.GetComponent<EHMinion>();
+        if (Minion != null)
+        {
+            if (Targets.Contains(Minion))
+            {
+                Targets.Remove(Minion);
+            }
+
+            if (Targets.Count > 0)
+            {
+                EnemyTarget = Targets[0];
+            }
+            else
+            {
+                EnemyTarget = null;
+            }
+        }
         
+    }
+
+    private void UpdateRotation()
+    {
+        Vector3 OffsetPosition = EnemyTarget.GetActorLocation() - GetActorLocation();
+        float YRotation = Mathf.Atan2(OffsetPosition.x, OffsetPosition.z) * Mathf.Rad2Deg;
+        Quaternion GoalRotation = Quaternion.Euler(-90, YRotation, 0);
+        RotationTransform.localRotation = Quaternion.Slerp(RotationTransform.localRotation, GoalRotation, RotationSpeed * Time.deltaTime);
     }
     #endregion monobehaviour methods
     
