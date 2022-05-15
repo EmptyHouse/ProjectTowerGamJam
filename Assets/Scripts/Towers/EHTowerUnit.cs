880d4ada11152ce8e3c700798cb46208ef8404b5
Ryan Andersen <ryan.andersen05@gmail.com>
2022-05-15 01:30:53 -0400

    Shooting added to the game

--- Assets/Scripts/Towers/EHTowerUnit.cs
+++ Assets/Scripts/Towers/EHTowerUnit.cs
@@ -6,12 +6,21 @@ using UnityEngine;
 public class EHTowerUnit : EHActor
 {
     [SerializeField]
+    private EHProjectiles ProjectileToFire;
+    [SerializeField]
     private float RotationSpeed = 100f;
     [SerializeField]
     private Transform RotationTransform;
 
     private List<EHActor> Targets = new List<EHActor>();
+    [SerializeField]
+    private List<Transform> LaunchPoints = new List<Transform>();
+    private int LaunchIndex;
     private EHActor EnemyTarget;
+    [SerializeField]
+    private float DelayFire = .1f;
+
+    private float TimeTillNextFire = 0;
     
     #region monobehaviour methods
     private void Update()
@@ -19,6 +28,12 @@ public class EHTowerUnit : EHActor
         if (EnemyTarget != null)
         {
             UpdateRotation();
+            if (TimeTillNextFire <= 0)
+            {
+                FireWeapon();
+            }
+
+            TimeTillNextFire -= Time.deltaTime;
         }
     }
 
@@ -63,6 +78,16 @@ public class EHTowerUnit : EHActor
         float YRotation = Mathf.Atan2(OffsetPosition.x, OffsetPosition.z) * Mathf.Rad2Deg;
         Quaternion GoalRotation = Quaternion.Euler(-90, YRotation, 0);
         RotationTransform.localRotation = Quaternion.Slerp(RotationTransform.localRotation, GoalRotation, RotationSpeed * Time.deltaTime);
+    }
+
+    private void FireWeapon()
+    {
+        TimeTillNextFire += DelayFire;
+        Transform LaunchTransform = LaunchPoints[LaunchIndex++];
+        LaunchIndex %= LaunchPoints.Count;
+        EHProjectiles Projectile = Instantiate(ProjectileToFire, LaunchTransform.position, Quaternion.identity);
+        Vector3 LaunchDirection = EnemyTarget.GetActorLocation() - LaunchTransform.position;
+        Projectile.LaunchProjectile(LaunchDirection);
     }
     #endregion monobehaviour methods
     
